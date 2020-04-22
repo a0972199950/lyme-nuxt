ac2029c47590e64ccf7c50d84410390708cae441
a0972199950 <a0972199950@gmail.com>
2020-04-22 20:41:01 +0800

    Add log to debug service registeration

--- plugins/registerSW.ts
+++ plugins/registerSW.ts
@@ -1,3 +1,7 @@
+console.log('執行plugin')
+
 if('serviceWorker' in navigator) {
+  console.log('service in navigator, 準備註冊sw.js')
   navigator.serviceWorker.register('/sw.js')
+  console.log('註冊完sw.js')
 }
